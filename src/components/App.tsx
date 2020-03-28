9f9a3d5c74161e31937ff39de6a133053f0dc3b8
Johnny Magrippis <j@magrippis.com>
2020-03-28 16:31:16 +0000

    bring in React, testing and easy local dev via create-react-app

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const App = () => <div>Healthy habits</div>
+
+export default App
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-const count = localStorage.getItem('count')
-
-document.getElementById('root').innerHTML = `Proof it works - count: ${count}`
-
-localStorage.setItem('count', `${parseInt(count) + 1}`)
