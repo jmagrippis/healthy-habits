1d8c294ee14850236eb72bb309b2ac8c78921587
Johnny Magrippis <j@magrippis.com>
2020-03-29 18:49:45 +0100

    remove values that can be selected later from categoriesToActivities

--- src/pickRandomActivities.ts
+++ src/pickRandomActivities.ts
@@ -1,11 +1,21 @@
 import { categoriesToActivities } from 'categoriesToActivities'
 import { sample, shuffle } from 'lodash'
+import { v4 as uuidv4 } from 'uuid'
 
-import { Activity } from 'types'
+import { Activity, Status } from 'types'
 
 export const pickRandomActivities = (): Activity[] =>
   shuffle(
-    Object.entries(categoriesToActivities).map(
-      ([, activities]) => sample(activities) as Activity
-    )
+    Object.entries(categoriesToActivities).map(([category, activities]) => {
+      const randomActivity = sample(activities)
+
+      if (!randomActivity)
+        throw new Error(`could not sample activity for ${category}`)
+
+      return {
+        ...randomActivity,
+        status: Status.Pending,
+        id: uuidv4(),
+      }
+    })
   )
