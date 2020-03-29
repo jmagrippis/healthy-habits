879a21e1be2864447ebc0b03e66ef826aba1671f
Alison Woodman <aliwoodman@gmail.com>
2020-03-29 11:41:01 +0100

    add checkboxes

--- src/components/Checklist/Item.tsx
+++ src/components/Checklist/Item.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import { Activity, Status } from 'types'
+import { Activity, Status } from "types";
 
 type Props = {
-  activity: Activity
-  onClick: (activity: Activity) => void
-}
+  activity: Activity;
+  onClick: (activity: Activity) => void;
+};
 
 const Container = styled.li`
   display: flex;
@@ -14,18 +14,26 @@ const Container = styled.li`
   padding: 1rem 2rem;
   margin-bottom: 1rem;
   cursor: pointer;
-`
+`;
 
 const Badge = styled.img`
   max-width: 5rem;
   border-radius: 50%;
   margin-right: 1rem;
-`
+`;
+
+const Checkbox = styled.img`
+  margin-left: 10px;
+`;
 
 export const Item = ({ activity, onClick }: Props) => (
   <Container onClick={() => onClick(activity)}>
     <Badge src={activity.badge} alt="badge icon for activity" />
     <span>{activity.label}</span>
-    {activity.status === Status.Done ? '✅' : activity.value}
+    {activity.status === Status.Done ? (
+      <Checkbox src={"checkbox-ticked.png"} />
+    ) : (
+      <Checkbox src={"checkbox-unticked.png"} />
+    )}
   </Container>
-)
+);
