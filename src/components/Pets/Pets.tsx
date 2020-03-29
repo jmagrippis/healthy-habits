c4249e6d53fa2d52d11093f6fd7814a207d29911
Alison Woodman <aliwoodman@gmail.com>
2020-03-29 12:29:17 +0100

    every two items completed earns a new pet

--- src/components/Pets/Pets.tsx
+++ src/components/Pets/Pets.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Pet } from "../../types";
+import styled from "styled-components";
 
 type Props = {
   totalActivitiesCompletedCount: number;
   pets: Pet[];
 };
 
-export const Pets = ({
-  totalActivitiesCompletedCount,
-  pets,
-}: Props) => (
-  <div>
-    {totalActivitiesCompletedCount >= 1 ? <img src={pets[0].image} /> : null}
-  </div>
+const PetImage = styled.img``;
+
+const getPets = (
+  totalActivitiesCompletedCount: number,
+  pets: Pet[]
+): ReactNode => {
+  const petCount = Math.floor(totalActivitiesCompletedCount / 2);
+  return pets
+    .slice(0, petCount)
+    .map((pet) => <PetImage src={pet.image} key={pet.id} />);
+};
+
+export const Pets = ({ totalActivitiesCompletedCount, pets }: Props) => (
+  <div>{getPets(totalActivitiesCompletedCount, pets)}</div>
 );
--- src/defaultPets.ts
+++ src/defaultPets.ts
@@ -3,5 +3,10 @@ export const defaultPets = [
     id: '1',
     label: 'mouse',
     image: 'mouse.gif',
+  },
+  {
+    id: '2',
+    label: 'cat',
+    image: 'cat.png',
   }
 ];
